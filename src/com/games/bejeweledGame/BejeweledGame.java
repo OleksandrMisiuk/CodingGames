6f085a61692f63ca67d313b07ba87681e6aefe98
Oleksandr Misiuk <santa.misuklord@gmail.com>
2020-01-04 16:51:36 +0200

    Bejeweled game.. Added pieces moving

--- src/com/games/bejeweledGame/BejeweledGame.java
+++ src/com/games/bejeweledGame/BejeweledGame.java
@@ -3,10 +3,13 @@ package com.games.bejeweledGame;
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.util.Random;
+import java.util.TimerTask;
 
 public class BejeweledGame extends JPanel {
 
@@ -19,10 +22,65 @@ public class BejeweledGame extends JPanel {
     private static String BACKGROUND_PATH = "C:/Developing/CodingMath/resources/bejeweled/background.png",
             GEMS_PATH = "C:/Developing/CodingMath/resources/bejeweled/gems.png";
 
+    private Point pos = new Point();
+    private int x0, y0, x, y, click;
+
     public BejeweledGame() {
         super();
         initGrid();
         initImages();
+
+        new java.util.Timer().scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                repaint();
+                piecesMove();
+            }
+        }, 100, 1000 / 40);
+
+
+        addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+                if (e.getButton() == MouseEvent.BUTTON1) {
+                    click++;
+                    pos.setLocation(e.getX() - offset.getX(), e.getY() - offset.getY());
+                    System.out.println(pos);
+                }
+            }
+        });
+    }
+
+    private void piecesMove() {
+        if (click == 1) {
+            x0 = pos.x / ts + 1;
+            y0 = pos.y / ts + 1;
+            System.out.println(x0 + " " + y0);
+        }
+        if (click == 2) {
+            x = pos.x / ts + 1;
+            y = pos.y / ts + 1;
+            if (Math.abs(x - x0) + Math.abs(y - y0) == 1) {
+                swapPieces(grid[y0][x0], grid[y][x]);
+                click = 0;
+            } else click = 1;
+        }
+
+
+        for (int i = 1; i <= 8; i++)
+            for (int j = 1; j <= 8; j++) {
+                Piece p = grid[i][j];
+                int dx, dy;
+                for (int n = 0; n < 4; n++)   // 4 - speed
+                {
+                    dx = p.getX() - p.getCol() * ts;
+                    dy = p.getY() - p.getRow() * ts;
+                    if (dx != 0) p.setX(p.getX() - dx / Math.abs(dx));
+                    if (dy != 0) p.setY(p.getY() - dy / Math.abs(dy));
+                }
+            }
+
     }
 
     @Override
@@ -55,6 +113,18 @@ public class BejeweledGame extends JPanel {
                 g2d.drawImage(gem, (int) (p.getX() + offset.getX() - ts), (int) (p.getY() + offset.getY() - ts), null);
             }
         }
+    }
+
+    private void swapPieces(Piece p1, Piece p2) {
+        int temp = p1.getCol();
+        p1.setCol(p2.getCol());
+        p2.setCol(temp);
+        temp = p1.getRow();
+        p1.setRow(p2.getRow());
+        p2.setRow(temp);
+        grid[p1.getRow()][p1.getCol()] = p1;
+        grid[p2.getRow()][p2.getCol()] = p2;
+
     }
 
     private void initImages() {
